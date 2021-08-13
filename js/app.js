b1cced121b557ab0d022cdd2ea3626681a854afa
Matthieu Lemesle <matthieulemesle@gmail.com>
2021-08-13 12:26:10 +0200

    Ajout du dossier backend, page index et fichiers scss

---
+++ js/app.js
@@ -0,0 +1,18 @@
+/* var btnNav = document.querySelector('.btn-nav');
+
+var bodyElement = document.getElementById("parent");
+
+// var paragraph = document.getElementById("child");
+// bodyElement.removeChild(paragraph);
+
+function textDisappears(){
+    bodyElement.remove(bodyElement);
+};
+
+var text = btnNav.addEventListener('click', textDisappears); */
+
+function presentation(){
+    console.log("Hello je suis Matthieu")
+}
+
+presentation();
