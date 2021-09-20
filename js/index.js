c94bf6a5d1d4feb982a8d7ea894603c80f5a07b8
Matthieu Lemesle <matthieulemesle@gmail.com>
2021-09-20 17:08:10 +0200

    All done till from validation

--- js/index.js
+++ js/index.js
@@ -1,67 +1,35 @@
-// const grid = document.querySelector('.products-grid');
-// const cardLink = document.createElement('a');
-// grid.appendChild(cardLink);
-// cardLink.classList.add('card');
-// const attrHref = document.createAttribute('href');
-// cardLink.setAttributeNode(attrHref);
-// cardLink.setAttribute('href', './pages/product.html');
-
-// const img = document.createElement('img');
-// cardLink.appendChild(img);
-// img.classList.add('card__img');
-// const attrAlt = document.createAttribute('alt');
-// img.setAttributeNode(attrAlt);
-// img.setAttribute('alt', 'Image d\'un ourson en peluche');
-// const attrSrc = document.createAttribute('src');
-// img.setAttributeNode(attrSrc);
-// img.setAttribute('src', './img/teddy.webp');
-
-// const cardButton = document.createElement('div');
-// cardButton.textContent = 'Voir le produit';
-// cardLink.appendChild(cardButton);
-// cardButton.classList.add('card__button', 'card__button--style');
-
-// const cardContent = document.createElement('div');
-// cardLink.appendChild(cardContent);
-// cardContent.classList.add('card__content');
-
-// /* const contentCard = document.querySelectorAll('.card__content'); // Selector */
-// const cardTitle = document.createElement('h2');
-
-// cardContent.appendChild(cardTitle);
-// cardTitle.classList.add('card__title');
-// cardTitle.textContent = 'Matthieu';
-
-// const cardPrice = document.createElement('div');
-// cardContent.appendChild(cardPrice);
-// cardPrice.classList.add('card__price');
-// cardPrice.textContent = '19.99€'; //.textcontent à la place de innerText
-
-// fetch
-
+// Appel à l'API pour récupérer les données de tous les produits //
 fetch('http://localhost:3000/api/teddies')
     .then(response => response.json())
     .then(dataTable => {
-
+        // Boucle pour itérer sur les arrays afin de récupérer les données de chacun des produits
         for (let i = 0; i < dataTable.length; i++) {
+
             const dataUrlImg = dataTable[i].imageUrl;
             const dataName = dataTable[i].name;
-            let dataPrice = dataTable[i].price;
-            let dataId = dataTable[i]._id;
+            const dataPrice = dataTable[i].price;
+            const dataId = dataTable[i]._id;
+
+            // Création du HTML en y insérant les valeurs des produits
             const grid = document.querySelector('.products-grid');
             const cardLink = document.createElement('a');
             grid.appendChild(cardLink);
             cardLink.classList.add('card');
             const attrHref = document.createAttribute('href');
             cardLink.setAttributeNode(attrHref);
+            // Attribution d'une URL unique à chacun des produits qui inclut leur ID
             cardLink.setAttribute('href', './pages/product.html?_id=' + dataId);
 
+            const divImg = document.createElement('div')
+            cardLink.appendChild(divImg);
+            divImg.classList.add('container__img')
+
             const img = document.createElement('img');
-            cardLink.appendChild(img);
+            divImg.appendChild(img);
             img.classList.add('card__img');
             const attrAlt = document.createAttribute('alt');
             img.setAttributeNode(attrAlt);
-            img.setAttribute('alt', 'Image d\'un ourson en peluche');
+            img.setAttribute('alt', `Image d'un ourson en peluche`);
             const attrSrc = document.createAttribute('src');
             img.setAttributeNode(attrSrc);
             img.setAttribute('src', dataUrlImg);
@@ -76,7 +44,6 @@ fetch('http://localhost:3000/api/teddies')
             cardContent.classList.add('card__content');
 
             const cardTitle = document.createElement('h2');
-
             cardContent.appendChild(cardTitle);
             cardTitle.classList.add('card__title');
             cardTitle.textContent = dataName;
@@ -84,13 +51,15 @@ fetch('http://localhost:3000/api/teddies')
             const cardPrice = document.createElement('div');
             cardContent.appendChild(cardPrice);
             cardPrice.classList.add('card__price');
+            // Formatage de l'affichage du prix
             let priceFormat = dataPrice / 100;
             cardPrice.textContent = priceFormat.toFixed([2]) + ' €';
         }
     })
+    // Affichage d'un message d'erreur si la requête API échoue
     .catch((err) => {
         const grid = document.querySelector('.products-grid');
         const errMsg = document.createElement('p');
         grid.appendChild(errMsg);
-        errMsg.textContent = 'Une erreur s\'est produite lors du chargement des produits'
+        errMsg.textContent = `Une erreur s'est produite lors du chargement des produits`
     });
--- js/product.js
+++ js/product.js
@@ -1,21 +1,30 @@
+// Création d'un objet URL 
 let urlId = new URL(window.location.href);
+// Récupération de l'ID du produit dans l'URL
 let urlProductId = urlId.searchParams.get('_id');
 
+// Requête API via l'ID du produit (contenu dans l'URL)
 fetch('http://localhost:3000/api/teddies/' + urlProductId)
     .then(response => response.json())
     .then(dataProduct => {
 
+        // Insertion des données de l'API dans le HTML
         const productImage = document.querySelector('.product__img--size');
         productImage.setAttribute('src', dataProduct.imageUrl);
+
         const productName = document.querySelector('.product__name');
         productName.textContent = dataProduct.name;
+
         const productPrice = document.querySelector('.product__price');
         let productPriceFormat = dataProduct.price / 100;
-        productPrice.textContent = productPriceFormat.toFixed([2]) + ' €'
+        productPrice.textContent = productPriceFormat.toFixed([2]) + ' €';
+
         const productDescription = document.querySelector('.product__description');
         productDescription.textContent = dataProduct.description;
 
+        // Boucle afin d'itérer sur les options de couleurs de chacun des produits
         for (let i = 0; i < dataProduct.colors.length; i++) {
+
             const productColorOption = dataProduct.colors[i];
             const productOption = document.querySelector('#color-select');
             const productSelection = document.createElement('option');
@@ -23,7 +32,55 @@ fetch('http://localhost:3000/api/teddies/' + urlProductId)
             productSelection.classList.add('product__option');
             const attrValue = document.createAttribute('value');
             productSelection.setAttributeNode(attrValue);
-            productSelection.setAttribute('value', productColorOption.toLowerCase())
+            productSelection.setAttribute('value', productColorOption.toLowerCase());
             productSelection.textContent = productColorOption;
         }
+
+
+        const productOption = document.querySelector('#color-select');
+        const addCart = document.querySelector('.product__button');
+        //Evenement au clic sur le bouton Ajouter au panier
+        addCart.addEventListener('click', e => {
+
+            const optionSelection = productOption.value;
+            // Condition pour vérifier si l'option a bien été choisie : Si elle ne l'est pas, une alerte est envoyée à l'utilisateur et le produit n'est pas ajouté au panier. Sinon un objet avec l'ID, l'option et la quantité du produit est crée.
+            if (optionSelection == 'null') {
+                e.preventDefault();
+                window.alert(`Vous devez sélectionner le coloris de l'article`);
+            } else {
+
+                //Création objet à intégrer dans le localstorage
+                let productValues = {
+                    productId: dataProduct._id,
+                    optionProduct: optionSelection,
+                    quantity: 1
+                };
+                //Vérifier si localstorage est vide ou contient des données 
+                let productInLocStor = JSON.parse(localStorage.getItem('product'));
+                // Si localstorage vide, création d'un tableau vide
+                if (!productInLocStor) {
+                    productInLocStor = [];
+                };
+
+                let c = 0
+                //Boucle pour itérer sur les objets du tableau
+                for (let p = 0; p < productInLocStor.length; p++) {
+
+                    //Si l'ID du produit dans le localstorage est égal à l'ID de l'objet que l'on veut ajouter au panier ET que leur option (couleur) est identique, alors j'incrémente +1 à la quantité
+                    if ((productInLocStor[p].productId == productValues.productId) && (productInLocStor[p].optionProduct == productValues.optionProduct)) {
+                        productInLocStor[p].quantity++;
+                        localStorage.setItem('product', JSON.stringify(productInLocStor));
+                        c = 1;
+
+                    }
+                }
+                if (c == 0) {
+                    //Insérer les données/objets dans le tableau
+                    productInLocStor.push(productValues);
+                    // Mettre les données dans le localstorage au format objet JSON
+                    localStorage.setItem('product', JSON.stringify(productInLocStor));
+                }
+            }
+        });
+
     });
